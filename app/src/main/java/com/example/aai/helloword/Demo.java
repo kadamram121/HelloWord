81e35bd3c1c6f8bf1eb4d1db4efcb30755b01e72
ramkadam <ramkadam.genie@gmail.com>
2017-01-20 21:44:03 +0530

    My New Class Add Demo...

---
+++ app/src/main/java/com/example/aai/helloword/Demo.java
@@ -0,0 +1,10 @@
+package com.example.aai.helloword;
+
+/**
+ * Created by Aai on 20-Jan-17.
+ */
+
+public class Demo {
+
+    //My demo Class Add
+}
--- app/src/main/java/com/example/aai/helloword/MainActivity.java
+++ app/src/main/java/com/example/aai/helloword/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Call Layout File................
         setContentView(R.layout.activity_main);
     }
 }
